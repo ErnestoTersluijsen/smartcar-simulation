a6d8e7666ecd3477be5ca2bf7da82ce4d5f56c4b
Ernesto Tersluijsen <eaw.tersluijsen@student.han.nl>
2024-10-06 18:10:22 +0200

    changed editor settings

---
+++ smartcar_simulation/include/smartcar_simulation/wheel_odometer.hpp
@@ -0,0 +1,14 @@
+#ifndef SMARTCAR_SIMULATION_WHEEL_ODOMETER_HPP
+#define SMARTCAR_SIMULATION_WHEEL_ODOMETER_HPP
+
+class wheel_odometer
+{
+  public:
+    wheel_odometer(/* args */);
+    ~wheel_odometer();
+
+  private:
+    /* data */
+};
+
+#endif
---
+++ smartcar_simulation/src/wheel_odometer.cpp
@@ -0,0 +1,9 @@
+#include "smartcar_simulation/wheel_odometer.hpp"
+
+wheel_odometer::wheel_odometer(/* args */)
+{
+}
+
+wheel_odometer::~wheel_odometer()
+{
+}
