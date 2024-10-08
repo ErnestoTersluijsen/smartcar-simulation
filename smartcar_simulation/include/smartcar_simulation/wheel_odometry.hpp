4e486fffe08fa153bba5458710a65e9278ca6749
Ernesto Tersluijsen <eaw.tersluijsen@student.han.nl>
2024-10-08 14:52:00 +0200

    amazing commit message
    idk what to name this commit message

--- smartcar_simulation/include/smartcar_simulation/wheel_odometry.hpp
+++ smartcar_simulation/include/smartcar_simulation/wheel_odometry.hpp
@@ -3,14 +3,15 @@
 
 #include "smartcar_msgs/msg/status.hpp"
 
-#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/clock.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 struct Position
 {
-    double phi = 0;
-    double x = 0;
-    double y = 0;
+    double phi;
+    double x;
+    double y;
     rclcpp::Time timestamp;
 };
 
@@ -26,18 +27,21 @@ class WheelOdometry : public rclcpp::Node
 
     double calc_linear_velocity(int32_t rpm, double wheel_diameter);
 
-    void update_position(int32_t rpm, double steering_angle, double delta_time);
-
+    void update_position(int32_t rpm, double steering_angle, rclcpp::Time current_time);
 
     rclcpp::Subscription<smartcar_msgs::msg::Status>::SharedPtr subscription_;
 
-    // rclcpp::Publisher<>::SharedPtr publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
 
     Position position_;
 
     double wheelbase_;
 
     double wheel_diameter_;
+
+    std::array<double, 36> pose_covariance_;
+    
+    std::array<double, 36> twist_covariance_;
 };
 
 #endif // SMARTCAR_SIMULATION_WHEEL_ODOMETER_HPP
--- smartcar_simulation/src/wheel_odometry.cpp
+++ smartcar_simulation/src/wheel_odometry.cpp
@@ -1,54 +1,75 @@
 #include "smartcar_simulation/wheel_odometry.hpp"
 
+#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <cmath>
 
-WheelOdometry::WheelOdometry() : Node("wheel_odometry"), wheelbase_(0.257), wheel_diameter_(0.064)
+WheelOdometry::WheelOdometry()
+    : Node("wheel_odometry"), wheelbase_(0.257), wheel_diameter_(0.064), pose_covariance_({1.0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 0.5}), twist_covariance_({0.1, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 100000.0, 0, 0, 0, 0, 0, 0, 0.1})
 {
     subscription_ = this->create_subscription<smartcar_msgs::msg::Status>("/smartcar/vehicle_status", 10, std::bind(&WheelOdometry::vehicle_status_callback, this, std::placeholders::_1));
+
+    publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/smartcar/wheel/odom", 10);
+
+    position_.timestamp = rclcpp::Time(this->get_clock()->now(), RCL_SYSTEM_TIME);
 }
 
 void WheelOdometry::vehicle_status_callback(const smartcar_msgs::msg::Status::SharedPtr status_msg)
 {
     RCLCPP_INFO(this->get_logger(), "status message rpm: %i", status_msg->engine_speed_rpm);
-    position_.timestamp = this->get_clock()->now();
+
+    update_position(status_msg->engine_speed_rpm, status_msg->steering_angle_rad, rclcpp::Time(this->get_clock()->now(), RCL_SYSTEM_TIME));
 }
 
 void WheelOdometry::update_position(int32_t rpm, double steering_angle, rclcpp::Time current_time)
 {
     rclcpp::Duration time_step = current_time - position_.timestamp;
+    position_.timestamp = current_time;
 
-    position_.phi + calc_angular_velocity(rpm, steering_angle) * time_step.seconds();
+    RCLCPP_INFO(this->get_logger(), "time between status message: %f", time_step.seconds());
 
-    position_.x + calc_linear_velocity(rpm, wheel_diameter_) * std::cos(calc_angular_velocity(rpm, steering_angle)) * time_step.seconds();
+    double linear_velocity = calc_linear_velocity(rpm, steering_angle);
+    double angular_velocity = calc_angular_velocity(rpm, steering_angle);
 
-    position_.y + calc_linear_velocity(rpm, wheel_diameter_) * std::sin(calc_angular_velocity(rpm, steering_angle)) * time_step.seconds();
+    double phi = position_.phi + angular_velocity * time_step.seconds();
+    double x = position_.x + linear_velocity * std::cos(phi) * time_step.seconds();
+    double y = position_.y + linear_velocity * std::sin(phi) * time_step.seconds();
+
+    position_.phi = phi;
+    position_.x = x;
+    position_.y = y;
+
+    nav_msgs::msg::Odometry message;
+
+    message.pose.pose.position.x = x;
+    message.pose.pose.position.y = y;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, phi);
+    message.pose.pose.orientation = tf2::toMsg(q);
+
+    message.pose.covariance = pose_covariance_;
+
+    message.twist.twist.linear.x = linear_velocity;
+    message.twist.twist.linear.y = 0.0;
+    message.twist.twist.linear.z = 0.0;
+
+    message.twist.twist.angular.z = angular_velocity;
+
+    message.twist.covariance = twist_covariance_;
+
+    publisher_->publish(message);
 }
-
-// double WheelOdometry::calc_phi(int32_t rpm, double steering_angle, double time_step)
-// {
-//     return prev_phi_ + calc_angular_velocity(rpm, steering_angle) * time_step;
-// }
-
-// double WheelOdometry::calc_x(int32_t rpm, double steering_angle, double time_step)
-// {
-//     return prev_x_ + calc_linear_velocity(rpm, wheel_diameter_) * std::cos(calc_angular_velocity(rpm, steering_angle)) * time_step;
-// }
-
-// double WheelOdometry::calc_y(int32_t rpm, double steering_angle, double time_step)
-// {
-//     return prev_y_ + calc_linear_velocity(rpm, wheel_diameter_) * std::sin(calc_angular_velocity(rpm, steering_angle)) * time_step;
-// }
 
 double WheelOdometry::calc_angular_velocity(int32_t rpm, double steering_angle)
 {
-    double angular_velocity = (calc_linear_velocity(rpm, wheel_diameter_) / wheelbase_) / std::tan(steering_angle);
-    return angular_velocity;
+    return (calc_linear_velocity(rpm, wheel_diameter_) / wheelbase_) / std::tan(steering_angle);
 }
 
 double WheelOdometry::calc_linear_velocity(int32_t rpm, double wheel_diameter)
 {
-    double linear_velocity = (rpm * M_PI * wheel_diameter) / 60;
-    return linear_velocity;
+    return (rpm * M_PI * wheel_diameter) / 60;
 }
