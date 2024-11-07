099cea08c34ad8ed5716b38b1eae54f8bad46b8b
Ernesto Tersluijsen <eaw.tersluijsen@student.han.nl>
2024-11-07 18:45:46 +0100

    added document and doxygen

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # smartcar-simulation
  
+## Guarantees
+
+This repository was build tested on Ubuntu 22.04 LTS with ROS2 Humble, other system configurations's quality can not be guarenteed.
+
+## Compilation Instructions
+
+To build this project a build script has been supplied. 
+
+The `build.sh` script runs `colcon build` with the required arguments.
+
+## Running Instructions
+
+To run this project multiple scripts have been supplied.
+
+Each of the launch files can be called by using the `start_<launch file name>.sh`, these scripts convert the xacro file to an URDF file, build the project, sources the `setup.bash` script, and runs the appropiate launch file.
--- smartcar_simulation/include/smartcar_simulation/joint_state_publisher.hpp
+++ smartcar_simulation/include/smartcar_simulation/joint_state_publisher.hpp
@@ -6,20 +6,51 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 
+/**
+ * @brief Joint state publisher for the smartcar's different joints.
+ *
+ */
 class JointStatePublisher : public rclcpp::Node
 {
   public:
+    /**
+     * @brief Construct a new Joint State Publisher object
+     *
+     */
     JointStatePublisher();
 
   private:
-    rclcpp::Subscription<smartcar_msgs::msg::Status>::SharedPtr subscription_;
-
-    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
-
+    /**
+     * @brief Callback function for when a status message is received.
+     *
+     * @param status_msg Status message content
+     */
     void vehicle_status_callback(const smartcar_msgs::msg::Status::SharedPtr status_msg);
 
+    /**
+     * @brief Updates the wheel position based on the status message content.
+     *
+     * @param engine_speed Engine speed of the smartcar.
+     * @param steering_angle Steering angle of the smartcar.
+     */
     void update_wheel_position(int32_t engine_speed, double steering_angle);
 
+    /**
+     * @brief Subscription for the smartcar status messages.
+     *
+     */
+    rclcpp::Subscription<smartcar_msgs::msg::Status>::SharedPtr subscription_;
+
+    /**
+     * @brief Publisher for the joint states.
+     *
+     */
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
+
+    /**
+     * @brief Message containing the states of the joints.
+     *
+     */
     sensor_msgs::msg::JointState msg_;
 };
 
